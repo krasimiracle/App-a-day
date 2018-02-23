b7d8c20b77a60ed2830c3a127ab17b71fddd2406
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2018-02-23 17:32:25 +0200

    Changed minSdk for material design purpose

--- 1.TypeText/app/src/main/java/com/stoyanov5/typetext/MainActivity.java
+++ 1.TypeText/app/src/main/java/com/stoyanov5/typetext/MainActivity.java
@@ -2,12 +2,22 @@ package com.stoyanov5.typetext;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mainTextView;
+
+    private EditText editText;
+
+    private Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
