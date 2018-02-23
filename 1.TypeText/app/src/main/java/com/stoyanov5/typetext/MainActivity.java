54ea475d657bb7110e36f19dc8702c4d2c82d871
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2018-02-23 17:44:38 +0200

    ConstraintLayout for the activity including TextView, EditText and Button.

--- 1.TypeText/app/src/main/java/com/stoyanov5/typetext/MainActivity.java
+++ 1.TypeText/app/src/main/java/com/stoyanov5/typetext/MainActivity.java
@@ -2,6 +2,7 @@ package com.stoyanov5.typetext;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,5 +20,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mainTextView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        submitButton = findViewById(R.id.button);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainTextView.setText(editText.getText().toString());
+            }
+        });
     }
 }
