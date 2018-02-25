2ed7b326e1d7d3563205e1a7b64eb7fdb265bbbd
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2018-02-25 15:56:59 +0200

    Timer with RxJava finished

--- 2.RxJavaTime/app/src/main/java/com/stoyanov5/rxjavatime/MainActivity.java
+++ 2.RxJavaTime/app/src/main/java/com/stoyanov5/rxjavatime/MainActivity.java
@@ -1,12 +1,27 @@
 package com.stoyanov5.rxjavatime;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Observable;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableEmitter;
+import io.reactivex.FlowableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
+
+    private Button observeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +29,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.text);
+        observeButton = findViewById(R.id.button);
+
+        observeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                runSchedulerOnButtonClicked();
+            }
+        });
+    }
+
+    void runSchedulerOnButtonClicked() {
+        Disposable observable = Flowable.interval(1, TimeUnit.SECONDS).map(v -> (v % 30) + 1)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(result -> textView.setText(result.toString()));
     }
 }
