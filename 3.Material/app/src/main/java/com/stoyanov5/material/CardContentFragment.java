bbfab4d92f2d27b711e1f5953c465eedeb26635f
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2018-02-27 18:14:49 +0200

    Added viewpager and fragments for each tab view

---
+++ 3.Material/app/src/main/java/com/stoyanov5/material/CardContentFragment.java
@@ -0,0 +1,19 @@
+package com.stoyanov5.material;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by B3f0r on 27-Feb-18.
+ */
+
+public class CardContentFragment extends android.support.v4.app.Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.item_card, null);
+    }
+}
--- 3.Material/app/src/main/java/com/stoyanov5/material/MainActivity.java
+++ 3.Material/app/src/main/java/com/stoyanov5/material/MainActivity.java
@@ -1,9 +1,18 @@
 package com.stoyanov5.material;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.ListFragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,12 +21,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TabLayout tabLayout = findViewById(R.id.tabs);
-        tabLayout.addTab(tabLayout.newTab().setText("First Tab"));
-        tabLayout.addTab(tabLayout.newTab().setText("Second Tab"));
-        tabLayout.addTab(tabLayout.newTab().setText("Third Tab"));
-
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+        adapter.addFragment(new ListContentFragment(),"List Fragment");
+        adapter.addFragment(new TileContentFragment(),"Tile Fragment");
+        adapter.addFragment(new CardContentFragment(),"Card Fragment");
+        viewPager.setAdapter(adapter);
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> fragmentList = new ArrayList<>();
+        private final List<String> fragmentTitleList = new ArrayList<>();
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            fragmentList.add(fragment);
+            fragmentTitleList.add(title);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitleList.get(position);
+        }
     }
 }
