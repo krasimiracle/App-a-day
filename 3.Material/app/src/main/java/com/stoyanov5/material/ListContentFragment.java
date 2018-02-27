9748989a330569ad17c96cf1f45b9c8ea2249534
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2018-02-27 20:28:42 +0200

    Added recyclerview layout

--- 3.Material/app/src/main/java/com/stoyanov5/material/ListContentFragment.java
+++ 3.Material/app/src/main/java/com/stoyanov5/material/ListContentFragment.java
@@ -3,6 +3,7 @@ package com.stoyanov5.material;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,5 +17,33 @@ public class ListContentFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.item_list, null);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.item_list, parent, false));
+        }
+    }
+
+    /**
+     * Adapter to display recycler view.
+     */
+    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 }
