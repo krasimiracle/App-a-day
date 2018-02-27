e8d640748c0f83ec0921cd1028a11787d26edfec
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2018-02-27 17:52:02 +0200

    Added Toolbar and Tabs layout

--- 3.Material/app/src/main/java/com/stoyanov5/material/MainActivity.java
+++ 3.Material/app/src/main/java/com/stoyanov5/material/MainActivity.java
@@ -1,7 +1,9 @@
 package com.stoyanov5.material;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.addTab(tabLayout.newTab().setText("First Tab"));
+        tabLayout.addTab(tabLayout.newTab().setText("Second Tab"));
+        tabLayout.addTab(tabLayout.newTab().setText("Third Tab"));
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
