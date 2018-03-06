e5e38853749b6915efd6d5ed6686597465fab159
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2018-03-06 18:40:43 +0200

    RecyclerView ViewHolder added

--- 4.MaterialColorPicker/app/src/main/java/com/stoyanov5/a4materialcolorpicker/PaletteDetailFragment.java
+++ 4.MaterialColorPicker/app/src/main/java/com/stoyanov5/a4materialcolorpicker/PaletteDetailFragment.java
@@ -3,10 +3,12 @@ package com.stoyanov5.a4materialcolorpicker;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by B3f0r on 06-Mar-18.
@@ -22,8 +24,20 @@ public class PaletteDetailFragment extends Fragment {
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
+        public TextView colorRankText;
+        public TextView colorHexText;
+
         public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.item_color,parent, false));
+            colorRankText = itemView.findViewById(R.id.color_rank);
+            colorHexText = itemView.findViewById(R.id.color_hex);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Set the color of the Primary/Accent containers
+                }
+            });
         }
     }
 }
