f7508ee2bdb88e1df0ac42f9b01875b21640b76d
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2018-03-06 14:15:35 +0200

    Added cardview/recyclerview dependencies, added fragment for detail color view

---
+++ 4.MaterialColorPicker/app/src/main/java/com/stoyanov5/a4materialcolorpicker/PaletteDetailActivity.java
@@ -0,0 +1,13 @@
+package com.stoyanov5.a4materialcolorpicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PaletteDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_palette_detail);
+    }
+}
---
+++ 4.MaterialColorPicker/app/src/main/java/com/stoyanov5/a4materialcolorpicker/PaletteDetailFragment.java
@@ -0,0 +1,29 @@
+package com.stoyanov5.a4materialcolorpicker;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by B3f0r on 06-Mar-18.
+ */
+
+public class PaletteDetailFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.item_color,parent, false));
+        }
+    }
+}
